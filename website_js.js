0b515af11947b09b21aad06908edf8ca106564f6
FugitiveSine <henrytimmons2004@gmail.com>
2023-05-11 18:43:27 -0700

    init

---
+++ website_js.js
@@ -0,0 +1,13 @@
+
+var updateDate = "5/11/2023";
+
+function alertButton() {
+    alert("Hello World!");
+}
+
+
+function footerContent(){
+    document.write("<p>Author: Henry Timmons</p>");
+    document.write("<p>Last Updated: " + updateDate + "</p>");
+}
+
