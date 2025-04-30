eb457b12f240b51172addc887f5bc36518c852ad
FugitiveSine <henrytimmons2004@gmail.com>
2025-04-30 14:10:15 -0700

    Redesigned: footer, rows, background

--- website_js.js
+++ website_js.js
@@ -1,5 +1,9 @@
-
-var updateDate = "5/28/2023";
+/*
+* 5/28/2023
+* 4/30/2025
+*
+*/
+var updateDate = "4/30/2025";
 
 function alertButton() {
     alert("Hello World!");
@@ -7,10 +11,27 @@ function alertButton() {
 
 
 function footerContent(){
-    document.write("<p>Author: Henry Timmons</p>");
-    document.write("<p>Last Updated: " + updateDate + "</p>");
+   const footer = document.getElementById("footerContainer");
+
+   footer.innerHTML = `
+   <p>Author: Henry Timmons</p>
+        <p style="display:inline-block">Last Updated: </p> <p style="display:inline-block" id="currentDate"></p>
+
+        <p style=" float: right; padding-right: 50px; text-align: right;"> 
+            <a href="https://www.linkedin.com/in/henry-timmons-14500423a/" style="color: white; text-decoration:none;"> 
+                <i class="fa fa-linkedin" style="font-size:20px"></i> LinkedIn
+            </a> 
+        </p>
+        
+        <p style=" float: right; padding-right: 50px; text-align: right;"> 
+            <a href="https://github.com/FugitiveSine" style="color: white; text-decoration:none;">
+                <i class="fa fa-github" style="font-size:20px"></i> Github
+            </a>
+        </p>
+        `;
+        document.getElementById("currentDate").innerText = updateDate; // updates the id of currentDate which is in html with the variable of updateDate thats in js
 }
 window.onload = function(){
-    document.getElementById("currentDate").innerHTML = updateDate;
+    footerContent();
 }
 
